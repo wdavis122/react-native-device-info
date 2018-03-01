wdavis122/react-native-device-info

commit 0da619dd1b6f68347992f4cd4bfa168d39a6283f

fix: enhance isTablet() on Android (#313)

android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -18,6 +18,7 @@ import android.webkit.WebSettings;
 import android.telephony.TelephonyManager;
 import android.text.format.Formatter;
 import android.app.ActivityManager;
+import android.util.DisplayMetrics;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -91,7 +92,19 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
 
   private Boolean isTablet() {
     int layout = getReactApplicationContext().getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
-    return layout == Configuration.SCREENLAYOUT_SIZE_LARGE || layout == Configuration.SCREENLAYOUT_SIZE_XLARGE;
+    if (layout != Configuration.SCREENLAYOUT_SIZE_LARGE && layout != Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+      return false;
+    }
+    DisplayMetrics metrics = new DisplayMetrics();
+    getCurrentActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    if (metrics.densityDpi == DisplayMetrics.DENSITY_DEFAULT
+            || metrics.densityDpi == DisplayMetrics.DENSITY_HIGH
+            || metrics.densityDpi == DisplayMetrics.DENSITY_MEDIUM
+            || metrics.densityDpi == DisplayMetrics.DENSITY_TV
+            || metrics.densityDpi == DisplayMetrics.DENSITY_XHIGH) {
+      return true;
+    }
+    return false;
   }
 
   private float fontScale() {

commit 75db678967fdf37e9c13d3bdc78117ae10d2b5cd

fix: Fix production build crash on Android introduced in #313
Co-authored-by: Mikael Palm <mikael@flashguru.se>

RNDeviceInfo.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "RNDeviceInfo"
-  s.version      = "0.17.2"
+  s.version      = "0.17.3"
   s.summary      = "Device Information for react-native"
 
   s.homepage     = "https://github.com/rebeccahughes/react-native-device-info"

android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -95,8 +95,8 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
     if (layout != Configuration.SCREENLAYOUT_SIZE_LARGE && layout != Configuration.SCREENLAYOUT_SIZE_XLARGE) {
       return false;
     }
-    DisplayMetrics metrics = new DisplayMetrics();
-    getCurrentActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+    final DisplayMetrics metrics = getReactApplicationContext().getResources().getDisplayMetrics();
     if (metrics.densityDpi == DisplayMetrics.DENSITY_DEFAULT
             || metrics.densityDpi == DisplayMetrics.DENSITY_HIGH
             || metrics.densityDpi == DisplayMetrics.DENSITY_MEDIUM